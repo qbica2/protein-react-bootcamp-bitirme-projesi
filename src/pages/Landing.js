e095f54ad64b39c3853b111c39a85100f6f33273
kubilay <kubidik27@gmail.com>
2022-04-28 16:59:29 +0300

    landing page navigation desinged

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+import style from "../styles/landing.module.scss";
+import Logo from "../constants/Logo";
 
 function Landing() {
+	// eslint-disable-next-line no-unused-vars
 	const navigate = useNavigate();
 	return (
-		<div>Landing
-			<button onClick={() => navigate("/register")}>register</button>
-			<button onClick={() => navigate("/login")}>login</button>
+		<div className={style.landing}>
+			<nav>
+				<div className={style.navbar}>
+					<div className={style.logo}>
+						<Logo width= {129} height= {42}/>
+					</div>
+					<div className={style.links}>
+						<a>Ürün Ekle</a>
+						<a>Hesabım</a>
+					</div>
+				</div>
+			</nav>
+			<div className={style.container}>
+			</div>
 		</div>
 	);
 }
