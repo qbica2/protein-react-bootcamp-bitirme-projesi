91046c4cc24fe17f86bda707df8e244e8aa2647b
kubilay <kubidik27@gmail.com>
2022-05-04 03:15:26 +0300

    checked - unchecked icon added to offer modal

--- src/components/OfferModal.js
+++ src/components/OfferModal.js
@@ -1,13 +1,32 @@
-import React from "react";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import style from "../styles/offermodal.module.scss";
 import CloseIcon from "../constants/icons/CloseIcon";
+import CheckedIcon from "../constants/icons/CheckedIcon";
+import UncheckedIcon from "../constants/icons/UncheckedIcon";
 
 
 function OfferModal({ closeModal, src}) {
 
-	const options = [20,30,40];
+	const [checked20, setChecked20] = useState(false);
+	const [checked30, setChecked30] = useState(false);
+	const [checked40, setChecked40] = useState(false);
 
+	const handleOfferClick = (value) => {
+		setChecked20(false);
+		setChecked30(false);
+		setChecked40(false);
+		if (value.type === 20) {
+			setChecked20(!checked20);
+		} else if (value.type === 30) {
+			setChecked30(!checked30);
+		} else if (value.type === 40) {
+			setChecked40(!checked40);
+		}
+	};
+	
 	return (
 		<div className={style.modal}> 
 			<div className={style.content}>
@@ -35,13 +54,25 @@ function OfferModal({ closeModal, src}) {
 					</div>
 				</div>
 				<form>
-					{
-						options.map(option => (
-							<div className={style.option} key={option} id={option}>
-								<div>icon</div>
-								%{option} Kadar Teklif Ver
-							</div>	))
-					}
+					{/* maplediğim zaman re render limit sorununa takılıyordu */}
+					<div className={style.option} onClick={()=>handleOfferClick({type: 20})}>
+						{
+							checked20 ? <CheckedIcon /> : <UncheckedIcon />
+						}
+						%20'si Kadar Teklif Ver
+					</div>
+					<div className={style.option} onClick={()=>handleOfferClick({type: 30})}>
+						{
+							checked30 ? <CheckedIcon /> : <UncheckedIcon />
+						}
+						%30'si Kadar Teklif Ver
+					</div>
+					<div className={style.option} onClick={()=>handleOfferClick({type: 40})}>
+						{
+							checked40 ? <CheckedIcon /> : <UncheckedIcon />
+						}
+						%40'si Kadar Teklif Ver
+					</div>
 					<div className={style.formGroup}>
 						<input type="number" id="price" placeholder="Teklif Belirle" />
 						<label htmlFor="price">TL</label>
