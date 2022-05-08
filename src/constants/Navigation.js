4b0171652a9280774c20c979fc9d3c98227660a3
kubilay <kubidik27@gmail.com>
2022-05-09 01:49:06 +0300

    navigation responsive

--- src/constants/Navigation.js
+++ src/constants/Navigation.js
@@ -14,7 +14,7 @@ function Navigation() {
 					<Logo width= {129} height= {42}/>
 				</div>
 				<div className={style.links}>
-					<a onClick={()=>navigate("/upload")}> <AddIcon width={13} height={13} color="#4B9CE2"/><span>Ürün Ekle</span></a>
+					<a onClick={()=>navigate("/upload")}> <AddIcon width={13} height={13} color="#4B9CE2"/><span className={style.mini}>Ürün Ekle</span></a>
 					<a onClick={()=>navigate("/account")}> <AvatarIcon width={13} height={13} color="#4B9CE2"/> <span>Hesabım</span></a>
 				</div>
 			</div>
